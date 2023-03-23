106cc3c94ee1cf9349c04a8c90a6fc0cb627c4e5
Miguel Vilhena <mvmories@gmail.com>
2023-03-23 20:42:12 +0100

    Updated server endpoint

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -24,12 +24,15 @@ const Home = () => {
       setLoading(true);
 
       try {
-        const response = await fetch("http://localhost:8080/api/v1/post", {
-          method: "GET",
-          headers: {
-            "Content-Type": "application/json",
-          },
-        });
+        const response = await fetch(
+          "https://mvmories-dall-e.onrender.com/api/v1/post",
+          {
+            method: "GET",
+            headers: {
+              "Content-Type": "application/json",
+            },
+          }
+        );
         if (response.ok) {
           const result = await response.json();
           // We are reversing the data to show the newest one at the top
